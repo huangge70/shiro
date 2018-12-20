007613579b7906e853f45bf25458539105c13da3
huangge70 <17801093123@163.com>
2018-12-20 12:59:44 +0800

    Initial commit

---
+++ src/main/java/com/mmall/demo2/ShiroConfiguration.java
@@ -0,0 +1,77 @@
+package com.mmall.demo2;
+
+import org.apache.shiro.cache.MemoryConstrainedCacheManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+
+@Configuration
+public class ShiroConfiguration {
+
+    @Bean("shiroFilter")
+    public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager manager) {
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+        bean.setSecurityManager(manager);
+
+        bean.setLoginUrl("/login");
+        bean.setSuccessUrl("/index");
+        bean.setUnauthorizedUrl("/unauthorized");
+
+        LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        //认证之后可以访问
+        filterChainDefinitionMap.put("/index", "authc");
+        //无需认证，直接访问
+        filterChainDefinitionMap.put("/login", "anon");
+        filterChainDefinitionMap.put("/loginUser", "anon");
+        //需要admin角色才能访问
+        filterChainDefinitionMap.put("/admin", "roles[admin]");
+        //需要edit权限
+        filterChainDefinitionMap.put("/edit", "perms[edit]");
+        filterChainDefinitionMap.put("/druid/**", "anon");
+        filterChainDefinitionMap.put("/**", "user");
+        bean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return bean;
+    }
+
+    @Bean("securityManager")
+    public SecurityManager securityManager(@Qualifier("authRealm") AuthRealm authRealm) {
+        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+        manager.setRealm(authRealm);
+        return manager;
+    }
+
+    @Bean("authRealm")
+    public AuthRealm authRealm(@Qualifier("credentialMatcher") CredentialMatcher matcher) {
+        AuthRealm authRealm = new AuthRealm();
+        //开启shiro缓存
+        authRealm.setCacheManager(new MemoryConstrainedCacheManager());
+        authRealm.setCredentialsMatcher(matcher);
+        return authRealm;
+    }
+
+    @Bean("credentialMatcher")
+    public CredentialMatcher credentialMatcher() {
+        return new CredentialMatcher();
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(@Qualifier("securityManager") SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+        advisor.setSecurityManager(securityManager);
+        return advisor;
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
+        creator.setProxyTargetClass(true);
+        return creator;
+    }
+}
